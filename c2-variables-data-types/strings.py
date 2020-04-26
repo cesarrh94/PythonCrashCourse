787ea42fdd3d0e0478183972ba81ac25d77b6071
cirh <94cesarrh@gmail.com>
2020-04-25 22:44:30 -0500

    chapter 2 - string section completed

---
+++ c2-variables-data-types/strings.py
@@ -0,0 +1,11 @@
+full_name = 'cesar ivan rodriguez hernandez'
+print('uppercase: ' + full_name.upper())
+print('lowercase: ' + full_name.lower())
+print('titlecase: ' + full_name.title())
+
+fname = 'cesar'
+lname = 'rodriguez'
+
+# usage of f-string 
+full_name = f'{fname} {lname}'
+print(f'\nHello, {full_name.title()}!')
---
+++ c2-variables-data-types/try_it_yourself/tiy2.py
@@ -0,0 +1,15 @@
+personal_msg = "Hi, How you doing?"
+print(personal_msg)
+
+name_cases = 'sasaki kojiro'
+print(name_cases.upper())
+print(name_cases.lower())
+print(name_cases.title())
+
+# Demon Slayer quotes XD!!!
+famous_quote = f'Once Jigoro Kuwajima said: "If you can only do one thing, hone it perfection. Hone it to the utmost limit."'
+print(famous_quote)
+
+famous_quote = f'Once Jigoro Kuwajima said: It\'s all right to cry. It\'s all right to run away. Just don\'t ever give up!'
+print(famous_quote)
+
