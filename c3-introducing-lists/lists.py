58254a050fc96416f117e9fe526b37a2390dfb0c
cirh-laptop-ibm <94cesarrh@gmail.com>
2021-03-23 13:24:45 -0600

    str funciton example to print list - chapter 3

--- c3-introducing-lists/lists.py
+++ c3-introducing-lists/lists.py
@@ -4,6 +4,9 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 """ I used a f-string in order to print the structure wether not I had to used 
 a str() function to convert the list to string!"""
 print(f'bicycles list: {bicycles}')
+# using str function to print the values of the list
+list_to_string = str(bicycles)
+print('content of list to string: ' + list_to_string)
 
 # print the first any element of the list using indexes
 print(f'first element of the list: {bicycles[0].title()}')
